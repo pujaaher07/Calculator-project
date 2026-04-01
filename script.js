e67d7ff2914abd11240e71c739bf10a8ae891b85
pujaaher <pujaaher0@gmail.com>
2026-04-01 22:31:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function add(value) {
+  document.getElementById("result").value += value;
+}
+
+function clearResult() {
+  document.getElementById("result").value = "";
+}
+
+function calculate() {
+  let result = document.getElementById("result").value;
+  document.getElementById("result").value = eval(result);
+}
